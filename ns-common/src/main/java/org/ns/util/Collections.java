6aba7e7f244950a9635cd240670b6902873eb9fc
stupak <stupak@funikov.kihdmn>
2014-03-05 10:59:42 +0400

    add EnumerationIterator

--- ns-common/src/main/java/org/ns/util/Collections.java
+++ ns-common/src/main/java/org/ns/util/Collections.java
@@ -1,6 +1,8 @@
 package org.ns.util;
 
 import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -22,5 +24,34 @@ public class Collections {
     
     public static <T> boolean isEmpty(T[] array) {
         return array == null || array.length == 0;
+    }
+    
+    public static<E> Iterator<E> iterator(Enumeration<E> enumeration) {
+        return new EnumarationIterator<>(enumeration);
+    }
+    
+    private static class EnumarationIterator<E> implements Iterator<E> {
+
+        private final Enumeration<E> enumeration;
+
+        public EnumarationIterator(Enumeration<E> enumeration) {
+            this.enumeration = enumeration;
+        }
+        
+        @Override
+        public boolean hasNext() {
+            return enumeration.hasMoreElements();
+        }
+
+        @Override
+        public E next() {
+            return enumeration.nextElement();
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+        
     }
 }
